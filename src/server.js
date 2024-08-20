46d6909e6535df5bd7633a4799925ad8d870b202
Fernanda Gomes <feh.cmrg@outlook.com>
2024-08-20 20:19:06 -0300

    fix: correcao infos

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello My App!')
+    res.send('Hello My server !!')
 })
 
 app.listen(3000, () => {
